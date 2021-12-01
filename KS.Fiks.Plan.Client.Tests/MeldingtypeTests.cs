80f29811e91031734d0e28a258590d2195abd261
Jarle Børsheim <jarle.borsheim@gmail.com>
2021-12-01 18:58:23 +0100

    La til Tests prosjekt og noen tester

---
+++ KS.Fiks.Plan.Client.Tests/MeldingtypeTests.cs
@@ -0,0 +1,28 @@
+using System.IO;
+using KS.Fiks.Plan.Client.Models;
+using NUnit.Framework;
+
+namespace KS.Fiks.Plan.Client.Tests
+{
+    public class MeldingtypeTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        
+        [Test]
+        [Ignore("Alt er ikke klart enda")]
+        [TestCase(FiksPlanMeldingtypeV2.FinnDispensasjoner)]
+        [TestCase(FiksPlanMeldingtypeV2.HentAktoerer)]
+        public void Gi_plan_schema_har_filnavn_tilsvarende_meldingstype_navn(string meldingstype)
+        {
+            Assert.IsTrue(SchemafileExistInSchemaFolder(meldingstype));
+        }
+
+        private static bool SchemafileExistInSchemaFolder(string meldingstype)
+        {
+            return File.Exists(Path.Combine("Schema", $"{meldingstype}.schema.json"));
+        }
+    }
+}
