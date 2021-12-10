ae94fdfcd3986bc8322666bbe61294886cd8b380
Per Ola Stålberg <olastaalberg@gmail.com>
2021-12-10 09:20:56 +0100

    GeoJson crs update

--- KS.Fiks.Plan.Client/Models/FiksPlanMeldingtypeV2.cs
+++ KS.Fiks.Plan.Client/Models/FiksPlanMeldingtypeV2.cs
@@ -26,6 +26,7 @@ namespace KS.Fiks.Plan.Client.Models
         public const string ResultatFinnPlanerForMatrikkelenhet = "no.ks.fiks.gi.plan.v2.innsyn.planerformatrikkelenhet.resultat";
         public const string ResultatFinnPlaner = "no.ks.fiks.gi.plan.v2.innsyn.planer.resultat";
         public const string ResultatFinnDispensasjoner = "no.ks.fiks.gi.plan.v2.innsyn.dispensasjoner.resultat";
+        public const string ResultatFinnPlanbehandlinger = "no.ks.fiks.gi.plan.v2.innsyn.planbehandlinger.resultat";
         public const string ResultatHentGjeldendePlanbestemmelser = "no.ks.fiks.gi.plan.v2.innsyn.gjeldendeplanbestemmelser.resultat";
         public const string ResultatHentAktoerer = "no.ks.fiks.gi.plan.v2.innsyn.aktoerer.resultat";
         public const string ResultatHentRelatertePlaner = "no.ks.fiks.gi.plan.v2.innsyn.relaterteplaner.resultat";
